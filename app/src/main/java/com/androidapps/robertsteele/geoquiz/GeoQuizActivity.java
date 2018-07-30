aec195073d19fd5d8e634dccd347a8b01129f4f2
robertsteele <steele.210@osu.edu>
2018-07-30 13:13:26 -0400

    Chapter 3 before challenges

--- app/src/main/java/com/androidapps/robertsteele/geoquiz/GeoQuizActivity.java
+++ app/src/main/java/com/androidapps/robertsteele/geoquiz/GeoQuizActivity.java
@@ -6,46 +6,58 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 
 public class GeoQuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
     private TextView mQuestionTextView;
+    private static final String TAG = "GeoQuizActivity";
+    private static final String KEY_INDEX = "index";
 
-    private Question[] mQuestions = new Question[] {
-        new Question(R.string.question_australia, true),
-                new Question(R.string.question_oceans, true),
-                new Question(R.string.question_mideast, false),
-                new Question(R.string.question_africa, false),
-                new Question(R.string.question_americas, true),
-                new Question(R.string.question_asia, true),
+    private Question[] mQuestions = new Question[]{
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
     };
 
     private int mQuestionIndex = 0;
-
 
     private void updateQuestion() {
         mQuestionIndex = (mQuestionIndex + 1) % mQuestions.length;
         mQuestionTextView.setText(mQuestions[mQuestionIndex].getMtextResId());
     }
 
-    private void checkAnswer(boolean userPressedTrue){
+    private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestions[mQuestionIndex].ismTrueAnswer();
         int resId = 0;
-        if(userPressedTrue == answerIsTrue){
+        if (userPressedTrue == answerIsTrue) {
             resId = R.string.correct_toast;
-        }
-        else{
+        } else {
             resId = R.string.incorrect_toast;
         }
         Toast.makeText(this, resId, Toast.LENGTH_SHORT).show();
     }
 
     @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d(TAG, "onSavedInstanceState Called");
+        savedInstanceState.putInt(KEY_INDEX, mQuestionIndex);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "The OnCreate callback function!");
         setContentView(R.layout.activity_geo_quiz);
+       if (savedInstanceState != null) {
+           savedInstanceState.getInt(KEY_INDEX,0);
+       }
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_buttton);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -67,11 +79,41 @@ public class GeoQuizActivity extends AppCompatActivity {
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mQuestionTextView.setText(mQuestions[mQuestionIndex].getMtextResId());
                 updateQuestion();
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart callback function");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume callback function");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "OnStop callback function");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "OnPause callback function");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
 }
 
 
